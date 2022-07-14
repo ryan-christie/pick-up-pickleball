72ed06dcba7a68fb54683f686bf83855697212c1
Ryan Christie <rcchristie@gmail.com>
2022-07-13 23:11:51 -0500

    Add and remove players from pool, start readme with requirements

---
+++ README.md
@@ -0,0 +1,27 @@
+## Design
+
+- Everyone uses same interface
+- Session based creates shareable url with random string
+- Remembers your session. If lost or want to share, use custom url
+- Add members
+- Option to set players never to play together
+- Easily enter results
+    - No entry will mark as a tie
+- Be able to run multiple games at once
+- Add members to the pool on the fly
+- Have active and inactive members
+- Indicate who's available on a giving day
+- Run multiple games at once
+- Make sure no-one sits or stays in too long
+- Edit player names
+- Show history
+- Edit scores
+
+
+
+### Future Fancy
+- Make adjustments on teams to play well or poorly together
+- Allow people to manage them self
+- Have different roles: head pickle, gherkin
+- keep ongoing history and use that to keep match ups varied and even
+- Add singles?
--- app.js
+++ app.js
@@ -1,6 +1,44 @@
 var app = new Vue({
     el: '#app',
     data: {
-      hello: 'Hello Vue!'
+      hello: 'Hello Vue!',
+      players: [],
+      disallowTeams: [],
+      newPlayer: ''
+    },
+    mounted: function() {
+        const localData = localStorage.getItem('pickleData');
+        if (localData) {
+            const data = JSON.parse(localData);
+            this.players = data.players;
+        }
+    },
+    methods: {
+        addPlayer: function(e) {
+            e.preventDefault();
+
+            const self = this;
+
+            const id = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+            self.players.push({id, name:self.newPlayer});
+            self.newPlayer = '';
+
+            self.saveToLocal();
+        },
+        removePlayer: function(playerData) {
+            // console.log(player);
+            const self = this;
+            const findPlayerIndex = this.players.findIndex(player => player.id === playerData.id);
+            if (findPlayerIndex > -1) {
+                self.players.splice(findPlayerIndex, 1);
+            }
+        },
+        clearStorage: function() {
+            localStorage.removeItem('pickleData');
+            this.players = [];
+        },
+        saveToLocal: function() {
+            localStorage.setItem('pickleData', JSON.stringify({players: self.players}));
+        }
     }
   })
