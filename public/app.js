69d492bc1b8ee54df7e742e47b24260f77bb45ab
Ryan Christie <rcchristie@gmail.com>
2022-07-29 10:44:01 -0500

    Added player management to component

--- public/app.js
+++ public/app.js
@@ -30,6 +30,8 @@ var app = new Vue({
             this.players = data.players;
             this.disallowList = data.disallowList;
         }
+
+        this.$root.$on('save-to-local', this.saveToLocal);
     },
     computed: {
         timesGrouped: function() {
@@ -66,94 +68,6 @@ var app = new Vue({
     methods: {
         createUUID: function() {
             return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
-        },
-        addPlayer: function(e) {
-            e.preventDefault();
-
-            const self = this;
-
-            const id = self.createUUID();
-            self.players.push({id, name:self.newPlayer, available: true});
-            self.newPlayer = '';
-
-            self.saveToLocal();
-        },
-        toggleAvailable: function(id) {
-            const self = this;
-            const index = self.players.findIndex(player => player.id === id);
-            self.players[index].available = !self.players[index].available;
-            self.saveToLocal();
-        },
-        removePlayer: function(id) {
-            const self = this;
-            const findPlayerIndex = this.players.findIndex(player => player.id === id);
-            if (findPlayerIndex > -1) {
-                self.players.splice(findPlayerIndex, 1);
-                self.saveToLocal();
-            }
-
-            let updateDisallowList = [];
-            self.disallowList.forEach((team, index) => {
-                if (team[0].id !== id && team[0].id) {
-                    updateDisallowList.push(team);
-                }
-            });
-            if (updateDisallowList.length !== self.disallowList.length) {
-                self.disallowList = updateDisallowList;
-                self.saveToLocal();
-            }
-        },
-        addToDisallow: function(id) {
-            const self = this;
-
-            if (self.disallowTeam.length > 1 || self.disallowTeam.findIndex(player => player.id === id) > -1) {
-                self.sendPopMsg((self.players.find((player) => player.id === id)).name + ' already added');
-                return;
-            }
-
-            const findPlayer = this.players.find(player => player.id === id);
-            if (findPlayer) {
-                self.disallowTeam.push(findPlayer);
-            }
-        },
-        removeFromDisallow: function(id) {
-            const self = this;
-            const findPlayerIndex = this.disallowTeam.findIndex(player => player.id === id);
-            if (findPlayerIndex > -1) {
-                self.disallowTeam.splice(findPlayerIndex, 1);
-            }
-        },
-        clearDisallowTeam: function() {
-            this.disallowTeam = [];
-        },
-        addToDisallowList: function() {
-            const self = this;
-
-            let id1 = self.disallowTeam[0].id;
-            let id2 = self.disallowTeam[1].id;
-
-            const findTeamIndex = self.disallowList.findIndex(team => (team[0].id === id1 || team[1].id === id1) && (team[0].id === id2 || team[1].id === id2) );
-
-            if (findTeamIndex > -1) {
-                self.sendPopMsg('Team is already part of the exclusion list');
-                return;
-            }
-
-            self.disallowList.push(self.disallowTeam);
-            self.saveToLocal();
-            self.disallowTeam = [];
-        },
-        removeFromDisallowList: function(team) {
-            const self = this;
-
-            let id1 = team[0].id;
-            let id2 = team[1].id;
-            
-            const findTeamIndex = self.disallowList.findIndex(team => (team[0].id === id1 || team[1].id === id1) && (team[0].id === id2 || team[1].id === id2) );
-            if (findTeamIndex > -1) {
-                self.disallowList.splice(findTeamIndex, 1);
-                self.saveToLocal();
-            }
         },
         createMatches: function() {
             const self = this;
