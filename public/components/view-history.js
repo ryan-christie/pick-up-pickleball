db0e96e781b2c810461eaba05c07b10a3aa44ffd
Ryan Christie <ryan.christie@icc.edu>
2023-06-27 15:06:30 -0500

    Updated stats with sorted table view

--- public/components/view-history.js
+++ public/components/view-history.js
@@ -11,14 +11,50 @@ Vue.component('view-history', {
         <div class="row">
             <h2>History</h2>
 
-            <div class="col">
+            <div class="col-12">
                 <h3>Players</h3>
-                <p v-for="(stats, id) in players">{{ playerName(id) }} | {{ stats.win }} - {{ stats.loss }} ({{ winPercent(stats.win, stats.loss) }})</p>
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Player</th>
+                            <th>Wins</th>
+                            <th>Losses</th>
+                            <th>Win %</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="stats in individualRecord">
+                            <td>{{ playerName(stats.id) }}</td>
+                            <td>{{ stats.win }}</td>
+                            <td>{{ stats.loss }}</td>
+                            <td>{{ stats.percent }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
 
-            <div class="col">
+            <div class="col-12">
                 <h3>Teams</h3>
-                <p v-for="(stats, teamID) in teams">{{ teamPlayerNames(teamID) }} | {{ stats.win }} - {{ stats.loss }} ({{ winPercent(stats.win, stats.loss) }})</p>
+                
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Player</th>
+                            <th>Wins</th>
+                            <th>Losses</th>
+                            <th>Win %</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="stats in teamRecord">
+                            <td>{{ teamPlayerNames(stats.id) }}</td>
+                            <td>{{ stats.win }}</td>
+                            <td>{{ stats.loss }}</td>
+                            <td>{{ stats.percent }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
 
             <div class="col-12">
@@ -50,6 +86,56 @@ Vue.component('view-history', {
 
                 return match;
             }).reverse()
+        },
+        individualRecord: function() {
+            const self = this;
+
+            let temp = [];
+            for (const key in self.players) {
+                if (self.players.hasOwnProperty(key)) {
+                    temp.push({
+                        id: key,
+                        win: self.players[key].win,
+                        loss: self.players[key].loss,
+                        percent: self.winPercent(self.players[key].win, self.players[key].loss)
+                    })
+                }
+            }
+
+            return temp.sort((a, b) => {
+                if (b.percent == a.percent) {
+                    if (b.win == a.win) {
+                        return self.playerName(b.id) < self.playerName(a.id) ? 1 : -1;
+                    }
+                    return b.win - a.win;
+                }
+                return b.percent - a.percent;
+            })
+        },
+        teamRecord: function() {
+            const self = this;
+    
+            let temp = [];
+            for (const key in self.teams) {
+                if (self.teams.hasOwnProperty(key)) {
+                    temp.push({
+                        id: key,
+                        win: self.teams[key].win,
+                        loss: self.teams[key].loss,
+                        percent: self.winPercent(self.teams[key].win, self.teams[key].loss)
+                    })
+                }
+            }
+    
+            return temp.sort((a, b) => {
+                if (b.percent == a.percent) {
+                    if (b.win == a.win) {
+                        return self.teamPlayerNames(b.id) < self.teamPlayerNames(a.id) ? 1 : -1;
+                    }
+                    return b.win - a.win;
+                }
+                return b.percent - a.percent;
+            })
         }
     },
     methods: {
